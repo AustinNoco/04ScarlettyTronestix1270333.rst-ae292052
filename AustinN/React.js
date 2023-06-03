e5846000a7b3498100a1da582951fcf9606c468f
aUsABuisnessman <tangleaustin78@gmail.com>
2023-06-02 20:51:22 -0500

    this for LOLz

--- AustinN/React.js
+++ AustinN/React.js
@@ -1,3 +1,58 @@
+#include(safemath)
+proxy <my api server>
 
-		
-											
+import {useStaticJsonRPC } from "./bin";
+import { useStaticJsonRPC } from "./hooks";
+const fs = require("./locals")
+const { ethers } = require("./locals")
+const { ethers } = require("ethers");
+const npmLodash = require('lodash');
+const projModulefromAnotherRepo = require('my-module-from-git');
+const npmLodash = require('lodash');
+const projModulefromAnotherRepo = require('my-module-from-git');
+async load(id) {
+    if (id === wasmHelper.id) {
+      return `export default ${wasmHelper.code}`;
+    }
+
+    if (!id.toLowerCase().endsWith(".wasm")) {
+      return;
+    }
+
+    
+  function mintItem(string memory tokenURI)
+  public
+  returns (uint256)
+{
+  bytes32 uriHash = keccak256(abi.encodePacked(tokenURI));
+
+  //make sure they are only minting something that is marked "forsale"
+  require(forSale[uriHash],"NOT FOR SALE");
+  forSale[uriHash]=false;
+
+  tokenStrength[uriHash] = uint8( (randomResult % 100)+1 );
+  randomResult=0;
+
+  _tokenIds.increment();
+
+  uint256 id = _tokenIds.current();
+  _mint(msg.sender, id);
+  _setTokenURI(id, tokenURI);
+
+  uriToTokenId[uriHash] = id;
+
+  return id;
+}
+}
+
+it('Etch event', async () => {
+    await wait(hh, delay_period)
+    const commitment = getCommitment(b32('zone1'), zone1)
+    await send(rootzone.hark, commitment, { value: ethers.utils.parseEther('1') })
+    const rx = await send(rootzone.etch, b32('salt'), b32('zone1'), zone1)
+    expectEvent(rx, "Etch", ['0x' + b32('zone1').toString('hex'), zone1])
+    await check_entry(dmap, rootzone.address, b32('zone1'), LOCK, padRight(zone1))
+}
+)
+
+keyfj4j113k / {% data variables.product.prodname_secret_scanning_caps %}
