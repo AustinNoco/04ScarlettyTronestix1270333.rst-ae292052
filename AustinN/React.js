fbf4b8c317a90da079b1eeccdf5d291653af8885
AustinN <104612281+aUsABuisnessman@users.noreply.github.com>
2024-02-16 15:08:26 -0800

    Update React.js

--- AustinN/React.js
+++ AustinN/React.js
@@ -5,6 +5,51 @@ import {useStaticJsonRPC } from "./bin";
 import { useStaticJsonRPC } from "./hooks";
   return id;
 }
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+
+// Base contract with ownership functionality
+contract Ownable {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+}
+
+// Child contract that inherits from Ownable
+contract Token is Ownable {
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balances;
+
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _initialSupply;
+        balances[msg.sender] = _initialSupply;
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        require(account != address(0), "Invalid address");
+        balances[account] += amount;
+        totalSupply += amount;
+    }
+
+    // Other token-related functions can be added here
+}
+keyfj4j113k;
 }
 
 it('Etch event', async () => {
