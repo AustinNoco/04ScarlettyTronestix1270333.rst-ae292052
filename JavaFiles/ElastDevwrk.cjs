897f895dfd2f382e7bd2a6353fdb4f5833b63853
Austin Hinojosa <tangleaustin78@gmail.com>
2024-02-20 11:54:36 -0800

    Update '''laragon.app.test.rb, BBdevBrowser5.txt, and 32 more files...

---
+++ JavaFiles/ElastDevwrk.cjs
@@ -0,0 +1,82 @@
+---
+name: Dev | Build And Deploy
+name: Build And Deploy Dev
+on:
+  push:
+    branches: [dev] 
+  workflow_dispatch:
+env:
+  REGISTRY: ghcr.io
+**************************
+{
+  "jsonrpc": "2.0",
+  "id": 1,
+  "method": "accountSubscribe",
+  "params": [
+    "CM78CPUeXjn8o3yroDHxUtKsZZgoy4GPkPPXfouKNH12",
+    {
+      "encoding": "jsonParsed",
+      "commitment": "finalized"
+    }
+  ]
+
+{ "jsonrpc": "2.0", "result": 23784, "id": 1 }
+
+
+{
+  "jsonrpc": "2.0",
+  "method": "accountNotification",
+  "params": {
+    "result": {
+      "context": {
+        "slot": 5199307
+      },
+      "value": {
+        "data": [
+          "11116bv5nS2h3y12kD1yUKeMZvGcKLSjQgX6BeV7u1FrjeJcKfsHPXHRDEHrBesJhZyqnnq9qJeUuF7WHxiuLuL5twc38w2TXNLxnDbjmuR",
+          "base58"
+        ],
+        "executable": false,
+        "lamports": 33594,
+        "owner": "11111111111111111111111111111111",
+        "rentEpoch": 635,
+        "space": 80
+      }
+    },
+    "subscription": 23784
+  }
+}
+
+PARSED JSON ENCODING:
+
+{
+  "jsonrpc": "2.0",
+  "method": "accountNotification",
+  "params": {
+    "result": {
+      "context": {
+        "slot": 5199307
+      },
+      "value": {
+        "data": {
+          "program": "nonce",
+          "parsed": {
+            "type": "initialized",
+            "info": {
+              "authority": "Bbqg1M4YVVfbhEzwA9SpC9FhsaG83YMTYoR4a8oTDLX",
+              "blockhash": "LUaQTmM7WbMRiATdMMHaRGakPtCkc2GHtH57STKXs6k",
+              "feeCalculator": {
+                "lamportsPerSignature": 5000
+              }
+            }
+          }
+        },
+        "executable": false,
+        "lamports": 33594,
+        "owner": "11111111111111111111111111111111",
+        "rentEpoch": 635,
+        "space": 80
+      }
+    },
+    "subscription": 23784
+  }
